2cf59487c6d3d3427f8c749b18e73b1e72f1e89f
Rojin4455 <rojinsaji35@gmail.com>
2025-10-04 00:33:56 +0530

    added allow null for coupon id

--- quote_app/serializers.py
+++ quote_app/serializers.py
@@ -433,7 +433,7 @@ class SubmitFinalQuoteSerializer(serializers.Serializer):
     selected_packages = SelectedPackageSerializer(many=True, required=False)
     additional_notes = serializers.CharField(max_length=1000, required=False, allow_blank=True)
     signature = serializers.CharField(required=False, allow_blank=True)
-    coupon_id = serializers.CharField(required=False, allow_blank=True)
+    coupon_id = serializers.CharField(required=False, allow_blank=True, allow_null=True)
     preferred_contact_method = serializers.ChoiceField(
         choices=[('email', 'Email'), ('phone', 'Phone'), ('both', 'Both')],
         default='email'
