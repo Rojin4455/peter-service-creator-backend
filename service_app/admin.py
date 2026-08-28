070670fdad96f7d10bd4b6e5ce4f25d2b0a56d67
Rony George <ronygeorgengit@gmail.com>
2026-08-28 17:27:58 +0530

    added all in one dashbord analytics endpoint for fetching data

--- service_app/admin.py
+++ service_app/admin.py
@@ -1,5 +1,12 @@
 from django.contrib import admin
-from .models import Package,GlobalSizePackage,SubQuestionPricing,QuestionPricing,OptionPricing
+from .models import (
+    Package,
+    GlobalSizePackage,
+    SubQuestionPricing,
+    QuestionPricing,
+    OptionPricing,
+    DashboardApiKey,
+)
 
 admin.site.register(Package)
 admin.site.register(GlobalSizePackage)
@@ -7,4 +14,10 @@ admin.site.register(SubQuestionPricing)
 admin.site.register(QuestionPricing)
 admin.site.register(OptionPricing)
 
-# Register your models here.
+
+@admin.register(DashboardApiKey)
+class DashboardApiKeyAdmin(admin.ModelAdmin):
+    list_display = ('name', 'key_prefix', 'is_active', 'created_at', 'last_used_at')
+    list_filter = ('is_active',)
+    readonly_fields = ('id', 'key_prefix', 'key_hash', 'created_at', 'last_used_at')
+    search_fields = ('name', 'key_prefix')
